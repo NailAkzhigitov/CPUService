4952a0294dbf50c69376ea77ae7e3635c2962638
Brahus <nakzhigitov@ya.ru>
2015-10-31 19:56:55 +0300

    Настройка службы

--- CPULoadService/Program.cs
+++ CPULoadService/Program.cs
@@ -16,7 +16,7 @@ namespace CPULoadService
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[] 
             { 
-                new Service1() 
+                new CPULoadService() 
             };
             ServiceBase.Run(ServicesToRun);
         }
